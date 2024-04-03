4791f842959a24938b583a050740b6f6fa56fa90
Raghavendra GH <68004393+raghav835@users.noreply.github.com>
2024-04-03 11:57:28 +0530

    changes

--- Real-Estate/src/components/Navbar.jsx
+++ Real-Estate/src/components/Navbar.jsx
@@ -3,8 +3,20 @@ import './navbar.scss'
 function Navbar() {
     return(
         <nav>
-            <div className="left">Left</div>
-            <div className="right">Right</div>
+            <div className="left">
+                <a href="/" className='logo'>
+                    <img src="./logo.png" alt="logo" />
+                    <span>NammaBengaluru</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+            </div>
+            <div className="right">
+                <a href="/">Sign Up</a>
+                <a href="/" className='login'>Sign In</a>
+            </div>
         </nav>
     )
 }
