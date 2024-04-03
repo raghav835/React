c0b7778b39c73a87e1d3cdffe57af48951f4a438
Raghavendra GH <68004393+raghav835@users.noreply.github.com>
2024-04-03 14:19:47 +0530

    ss
    sass

--- Real-Estate/src/components/Navbar.jsx
+++ Real-Estate/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './navbar.scss'
 
 function Navbar() {
+    const[open,setOpen] = useState(false);
+
     return(
         <nav>
             <div className="left">
@@ -16,6 +19,21 @@ function Navbar() {
             <div className="right">
                 <a href="/">Sign Up</a>
                 <a href="/" className='login'>Sign In</a>
+
+                <div className="menuIcon">
+                    <img src="/menu.png" 
+                    alt="" 
+                    onClick={() => setOpen((prev) => !prev)} />
+                </div>
+
+                <div className={open ? 'menu active' : 'menu'}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sign In</a>
+                    <a href="/">Sign Up</a>
+                </div>
             </div>
         </nav>
     )
